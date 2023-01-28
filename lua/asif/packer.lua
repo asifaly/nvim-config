6c178b3e70eda95f16fa5c3653f5b34718dd3491
asifaly <asif@venzotechnologies.com>
2023-01-28 15:22:12 +0530

    remove unnecessary copilot.lua config file

--- lua/asif/packer.lua
+++ lua/asif/packer.lua
@@ -37,9 +37,9 @@ return require("packer").startup(function(use)
 	})
 	use({
 		"jose-elias-alvarez/null-ls.nvim",
-		config = function()
-			require("null-ls").setup()
-		end,
+		-- config = function()
+		-- 	require("null-ls").setup()
+		-- end,
 		requires = { "nvim-lua/plenary.nvim" },
 	})
 	use({
